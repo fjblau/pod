3b37ef00e62a2a9e9f05c9fa2e1fa99e7ecccd80
Frank Blau <fjblau@gmail.com>
2018-05-01 21:20:11 +0200

    Updated restart script

--- restart.sh
+++ restart.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+archiveFile="pod@$1.bna"
 
 composer archive create -t dir -n .
-composer network install -c peerAdmin@hlfv1 -a pod@0.0.5.bna
-composer network upgrade -c peeradmin@hlfv1 -n pod -V 0.0.5
-# Install networkadmin card with name admin
+composer network install -c peerAdmin@hlfv1 -a $archiveFile
+composer network upgrade -c peeradmin@hlfv1 -n pod -V $1
 composer-rest-server -c admin@pod -n never -w true
 
 # Fresh Install Only
 # composer network install -c PeerAdmin@hlfv1  -a pod.bna
-# composer network start --card PeerAdmin@hlfv1 --networkAdmin admin  --networkName pod --networkVersion 0.0.5 --networkAdminEnrollSecret adminpw  --file networkadmin.card
+# composer network start --card PeerAdmin@hlfv1 --networkAdmin admin  --networkName pod --networkVersion $1 --networkAdminEnrollSecret adminpw  --file networkadmin.card
