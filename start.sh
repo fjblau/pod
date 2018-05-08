c7056f4842d8f85f3e99fe0928cba89705a57dbd
fjblau <fjblau@gmail.com>
2018-05-08 14:38:58 +0200

    update version

--- start.sh
+++ start.sh
@@ -21,9 +21,6 @@ archiveFile="pod@$1.bna"
 
 composer archive create -t dir -n .
 composer network install -c peerAdmin@hlfv1 -a $archiveFile
-composer network upgrade -c peeradmin@hlfv1 -n pod -V $1
+composer network start --card PeerAdmin@hlfv1 --networkAdmin admin  --networkName pod --networkVersion $1 --networkAdminEnrollSecret adminpw  --file networkadmin.card
+
 composer-rest-server -c admin@pod -n never -w true
-
-# Fresh Install Only
-# composer network start --card PeerAdmin@hlfv1 --networkAdmin admin  --networkName pod --networkVersion 0.0.24 --networkAdminEnrollSecret adminpw  --file networkadmin.card
-
